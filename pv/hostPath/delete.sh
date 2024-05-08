4aa0e5646af8770515288592b4b4d649d9520108
YS <yunsikpyun@gmail.com>
2024-05-08 18:30:09 +0900

    수정

--- pv/hostPath/delete.sh
+++ pv/hostPath/delete.sh
@@ -1,10 +1,10 @@
 kubectl delete pv <pv_name> --grace-period=0 --force
 kubectl patch pv <pv_name> -p '{"metadata": {"finalizers": null}}'
 
+kubectl patch pv <pv_name> -p '{\"metadata\": {\"finalizers\": null}}' # <== 윈도우에서는 큰따옴표 앞에 \ 을 붙여야된다.
+
 kubectl delete pvc <pvc_name> --grace-period=0 --force
 kubectl patch pvc <pvc_name> -p '{"metadata": {"finalizers": null}}'
-
-
 
 kubectl delete pvc data-apache-airflow-postgresql-0 --grace-period=0 --force
 kubectl patch pvc data-apache-airflow-postgresql-0 -p '{"metadata": {"finalizers": null}}'
